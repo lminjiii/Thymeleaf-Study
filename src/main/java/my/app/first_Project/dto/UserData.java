86ec89d4af4fdf2aff3d3f072ab065ece353573f
lminjiii <lminjiiiii@gmail.com>
2025-02-06 12:18:54 +0900

    - 스프링부트 타임리프 테스트

---
+++ src/main/java/my/app/first_Project/dto/UserData.java
@@ -0,0 +1,43 @@
+package my.app.first_Project.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+//@RequiredArgsConstructor
+public class UserData {
+    private String userName;
+    private int userAge;
+    private String userAddress;
+
+    // getter, settet, toString
+    // 기본 생성자, 전체 생성자, 일부 생성자
+
+//    @AllArgsConstructor
+//    public UserData(String userName, int userAge, String userAddress) {
+//        this.userName = userName;
+//        this.userAge = userAge;
+//        this.userAddress = userAddress;
+//    }
+//    @NoArgsConstructor
+//    public UserData() {
+//
+//    }
+//    @RequiredArgsConstructor
+//    public UserData(String userName) {
+//        this.userName = userName;
+//    }
+//    public UserData(String userName, int userAge) {
+//        this.userName = userName;
+//        this.userAge = userAge;
+//    }
+//    @AllArgsConstructor
+//    @NoArgsConstructor
+//    @RequiredArgsConstructor
+//    @Getter
+//    @Setter
+//    @Data= getter, setter, reqargcon...
+//    @ToString
+//    @EqualsAndHashCode
+}
